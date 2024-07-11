a4b2fba12f495e3394efb57d4e1c96761a978e5b
Ulises Wolfzun <wolfzunulises@gmail.com>
2024-07-11 15:42:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,10 @@
-from typing import Optional
-from fastapi import FastAPI, File, UploadFile, Form
+from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
-import tensorflow as tf
-from tensorflow import keras
 from keras.models import load_model
 from tensorflow.keras.preprocessing.image import img_to_array
-from keras.preprocessing import image
 import numpy as np
 from PIL import Image
 import io
-import requests
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 origins = [
     "*",
@@ -21,12 +12,6 @@ origins = [
     "http://localhost:3000",
     "https://hamec.vercel.app/"
 ]
-
-modelId = os.getenv("MODEL_ID")
-
-url = f"https://drive.google.com/uc?id={modelId}"
-
-downloaded = False
 
 app = FastAPI(title="ECG Classification API", description="API para clasificar ECGs", version="0.1.0")
 
@@ -38,24 +23,6 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-async def download_model():
-    global downloaded
-    print("Descargando modelo")
-    try:
-        response = requests.get(url)
-        with open("model1.h5", 'wb') as f:
-            f.write(response.content)
-        downloaded = True
-        return {"message": "Modelo descargado"}
-    except Exception as e:
-        return {"error": str(e)}
-
-@app.on_event("startup")
-async def startup_event():
-    global downloaded 
-    downloaded = True
-    await download_model()
-
 
 @app.get("/")
 async def root():
@@ -63,14 +30,7 @@ async def root():
 
 @app.post("/api/predict/")
 async def predict(file: UploadFile = File(...)):
-    global downloaded
-    if (not UploadFile):
-        return {'File not uploaded'}
-    if (not downloaded):
-        print("Modelo no disponible...")
-        await download_model()
-        print("Modelo descargado")
-    print("Modelo ecnontrado")
+    print("api predict")
     try:
         model = load_model("model1.h5")
         print("modelo cargado")
